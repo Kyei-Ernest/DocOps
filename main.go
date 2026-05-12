654166eb420ba32de87dc611d8d6264fafac508d
Kyei-Ernest <ernestkyei101@gmail.com>
2026-05-12 16:04:02 +0000

    empty main

--- main.go
+++ main.go
@@ -1,17 +1,6 @@
 package main
 
-import "fmt"
 
 func main() {
-    type contextKey struct{ name string }
-
-    var (
-	// KEKKey is the context key under which the session KEK ([]byte) is stored.
-	KEKKey = &contextKey{"kek"}
-
-	// UserIDKey is the context key under which the authenticated user ID (string) is stored.
-	UserIDKey = &contextKey{"userID"}
-)
-
-fmt.Println(KEKKey.name, UserIDKey.name)
+    //welcome to docops
 }
